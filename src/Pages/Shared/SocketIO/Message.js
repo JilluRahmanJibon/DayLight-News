258b45b51150c07efe14deda48f16613b506c338
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-07 00:09:53 +0600

    message and  app css add

---
+++ src/Pages/Shared/SocketIO/Message.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../../Context/AuthContext";
+import Chat from "./Chat";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+const Message = () => {
+  const { user } = useContext(AuthContext);
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+};
+
+export default Message;
