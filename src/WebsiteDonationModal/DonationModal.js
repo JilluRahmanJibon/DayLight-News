e05cad9af84ecb88c1bb8982518e27bb30cf2247
shariful4345 <mdsajeeb4345@gmail.com>
2023-02-16 11:43:06 +0600

    modal update

--- src/WebsiteDonationModal/DonationModal.js
+++ src/WebsiteDonationModal/DonationModal.js
@@ -13,70 +13,58 @@ const DonationModal = () => {
         return () => clearTimeout(timeout);
     }, []);
 
+    // <button
+    //     className="bg-transparent border-0 text-black float-right"
+    //     onClick={() => setShowModal(false)}
+    // >
+    //     <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+    //         x
+    //     </span>
+    // </button>
+
 
     return (
         <div >
             {
-                showModal && 
+                showModal &&
                 <>
                     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                    <h3 className="text-3xl font=semibold">General Info</h3>
-                                    <button
-                                        className="bg-transparent border-0 text-black float-right"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
-                                            x
-                                        </span>
-                                    </button>
+                        <div className="relative w-11/12 my-6 mx-auto max-w-3xl">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full  bg-base-100 outline-none focus:outline-none">
+                                <button
+                                    onClick={() => setShowModal(false)}
+                                    className='flex flex-row-reverse pr-4 pb-0 text-3xl font-semibold hover:text-red-600'>
+                                    x
+                                </button>
+                                <div className="card sm:card-side shadow-xl px-4 pb-3">
+                                    <figure>
+                                        <img
+                                            className='sm:w-96 w-full  rounded-0'
+                                            src="https://i.ibb.co/JdrV0mx/photo-1579621970795-87facc2f976d-ixlib-rb-4-0.jpg" alt="Album" />
+                                    </figure>
+                                    <div className="card-body font-serif text-black">
+                                        <h2 className="card-title md:text-4xl text-2xl">Please Donate Some Money</h2>
+                                        <p className='text-xl'>If you donate some money please click  the  button and share it</p>
+                                        <div className="bg-red-100  w-full h-[2px] mx-auto sm:block    mt-2"></div>
+
+                                        <div className="card-actions ">
+                                            <button
+                                                className=" py-1 w-full text-xl font-semibold hover:bg-green-600 rounded-md bg-red-200">
+                                                Click
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="relative p-6 flex-auto">
-                                    <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            First Name
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            Last Name
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            Address
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            City..............
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
-                                    </form>
-                                </div>
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
+
+
                             </div>
                         </div>
                     </div>
                 </>
 
-               
+
             }
-            
+
         </div>
     );
 };
