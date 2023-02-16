95e5cb37c8fa231710ae7918410d69167c7f1cad
shariful4345 <mdsajeeb4345@gmail.com>
2023-02-16 12:16:07 +0600

    form icon update

--- src/WebsiteDonationModal/DonationModal.js
+++ src/WebsiteDonationModal/DonationModal.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import DonationModalCard from './DonationModalCard';
+import { BiDonateBlood } from 'react-icons/bi'
 
 const DonationModal = () => {
     const [showModal, setShowModal] = useState(false)
@@ -30,7 +31,7 @@ const DonationModal = () => {
                 <>
                     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-11/12 my-6 mx-auto max-w-3xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full  bg-base-100 outline-none focus:outline-none">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full  bg-gray-100 outline-none focus:outline-none">
                                 <button
                                     onClick={() => setShowModal(false)}
                                     className='flex flex-row-reverse pr-4 pb-0 text-3xl font-semibold hover:text-red-600'>
@@ -43,13 +44,16 @@ const DonationModal = () => {
                                             src="https://i.ibb.co/JdrV0mx/photo-1579621970795-87facc2f976d-ixlib-rb-4-0.jpg" alt="Album" />
                                     </figure>
                                     <div className="card-body font-serif text-black">
-                                        <h2 className="card-title md:text-4xl text-2xl">Please Donate Some Money</h2>
+                                        <h2
+                                            className="card-title md:text-4xl text-xl text-inline">
+                                            Please Donate Some Money!
+                                        </h2>
                                         <p className='text-xl'>If you donate some money please click  the  button and share it</p>
                                         <div className="bg-red-100  w-full h-[2px] mx-auto sm:block    mt-2"></div>
 
                                         <div className="card-actions ">
                                             <button
-                                                className=" py-1 w-full text-xl font-semibold hover:bg-green-600 rounded-md bg-red-200">
+                                                className=" flex items-center justify-center gap-2 py-1 w-full text-xl font-semibold hover:bg-green-600 rounded-md bg-red-200">
                                                 Click
                                             </button>
                                         </div>
